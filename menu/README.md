34e074f82097af8109ca5d1f0d4eaf97d056ddc8
Breno Baptista <brenobaptista@protonmail.com>
2022-05-28 02:09:06 -0300

    Added menu project

---
+++ menu/README.md
@@ -0,0 +1,5 @@
+# Menu
+
+> Menu made with LÖVE
+
+![Preview](https://github.com/brenobaptista/lua-love2d/blob/main/previews/menu.mp4)
---
+++ menu/main.lua
@@ -0,0 +1,30 @@
+require("buttons")
+
+function love.load()
+  buttons:load("Start Game", function() print("Starting game") end)
+  buttons:load("Load Game", function() print("Loading game") end)
+  buttons:load("Settings", function() print("Opening settings") end)
+  buttons:load("Exit", function() love.event.quit() end)
+end
+
+function love.update()
+  local mouseX, mouseY = love.mouse.getPosition()
+
+  buttons.loop(function(button)
+    buttons:update(button, mouseX, mouseY)
+  end)
+
+  buttons:setCursor()
+end
+
+function love.draw()
+  love.graphics.setBackgroundColor(love.math.colorFromBytes(40, 42, 54))
+
+  buttons.loop(function(button)
+    buttons:draw(button)
+  end)
+end
+
+function love.mousepressed()
+  buttons.handleClick()
+end
