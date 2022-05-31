1cccaae3d50764ad40f93e15824ea1bb801dd687
Breno Baptista <brenobaptista@protonmail.com>
2022-05-30 22:44:37 -0300

    Implemented mixer library and updated README

--- mixer/README.md
+++ mixer/README.md
@@ -2,21 +2,37 @@
 
 > A wrapper for the audio module of LÖVE
 
-## Loading audio
+This library is inspired by `pygame.mixer`, the pygame module for audio.
+
+## Music
+
+Music refers to longer tracks of audio, like background music or songs.
 
 ```lua
-mixer.load('blip', 'audio/blip-sound.wav', 'static')
-mixer.load('music', 'audio/game-music.mp3', 'stream', true)
+  mixer.loadMusic(name, filename, loop?, volume?)
+  mixer.playMusic(name)
+  mixer.pauseMusic(name)
+  mixer.stopMusic(name)
+  mixer.unloadMusic(name)
 ```
 
-## Playing audio
+## Sound
+
+Sound refers to small side effects such as crash noises or beeps, generally anything just a few seconds long.
 
 ```lua
-mixer.play('music')
+  mixer.loadSound(name, filename, volume?)
+  mixer.playSound(name)
+  mixer.muteSound(name)
+  mixer.unmuteSound(name)
+  mixer.unloadSound(name)
 ```
 
-## Stopping audio
+## Volume
+
+There are three general types of volumes: `master`, `music` and `sound`.
+The default value is 1 (100%), but it can be changed using the `setVolume` method.
 
 ```lua
-mixer.stop('music')
+  mixer.setVolume(type, volume)
 ```
--- mixer/mixer.lua
+++ mixer/mixer.lua
@@ -1,17 +1,76 @@
 local mixer = {}
-local mixerState = {}
+local musicState = {}
+local soundState = {}
+local mutedSounds = {}
+local volumes = {
+  master = 1,
+  music = 1,
+  sound = 1
+}
 
-function mixer.load(name, filename, type, shouldLoop)
-  mixerState[name] = love.audio.newSource(filename, type)
-  mixerState[name]:setLooping(shouldLoop or false)
+function mixer.setVolume(type, volume)
+  local oldVolume = volumes[type]
+  volumes[type] = volume
+
+  for name, music in pairs(musicState) do
+    local currentVolume = music:getVolume()
+    musicState[name]:setVolume(currentVolume * volume / oldVolume)
+  end
+
+  for name, sound in pairs(soundState) do
+    local currentVolume = sound:getVolume()
+    soundState[name]:setVolume(currentVolume * volume / oldVolume)
+  end
+
+  for name, mutedSound in pairs(mutedSounds) do
+    local currentVolume = mutedSound:getVolume()
+    mutedSounds[name]:setVolume(currentVolume * volume / oldVolume)
+  end
 end
 
-function mixer.play(name)
-  mixerState[name]:play()
+function mixer.loadMusic(name, filename, loop, volume)
+  musicState[name] = love.audio.newSource(filename, 'stream')
+  musicState[name]:setLooping(loop or false)
+  musicState[name]:setVolume(volumes.master * volumes.sound * (volume or 1))
 end
 
-function mixer.stop(name)
-  mixerState[name]:stop()
+function mixer.playMusic(name)
+  musicState[name]:play()
+end
+
+function mixer.pauseMusic(name)
+  musicState[name]:pause()
+end
+
+function mixer.stopMusic(name)
+  musicState[name]:stop()
+end
+
+function mixer.unloadMusic(name)
+  musicState[name] = nil
+end
+
+function mixer.loadSound(name, filename, volume)
+  soundState[name] = love.audio.newSource(filename, 'static')
+  soundState[name]:setVolume(volumes.master * volumes.sound * (volume or 1))
+end
+
+function mixer.playSound(name)
+  soundState[name]:play()
+end
+
+function mixer.muteSound(name)
+  mutedSounds[name] = soundState[name]:getVolume()
+  soundState[name]:setVolume(0)
+end
+
+function mixer.unmuteSound(name)
+  soundState[name]:setVolume(mutedSounds[name])
+  mutedSounds[name] = nil
+end
+
+function mixer.unloadSound(name)
+  soundState[name] = nil
 end
 
 return mixer
