881ad6ffdabf185668131987074bbca5979757f2
Breno Baptista <47641641+brenobaptista@users.noreply.github.com>
2024-08-07 00:24:33 -0300

    Improve pause screen

--- crushing-balls/src/pause.lua
+++ crushing-balls/src/pause.lua
@@ -11,11 +11,11 @@ function pause:draw()
   if self.isPaused then
     love.graphics.push("all")
 
-    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.setColor(0, 0, 0, 0.3)
     love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 
     love.graphics.setColor(love.math.colorFromBytes(248, 248, 242))
-    love.graphics.setNewFont(20)
+    love.graphics.setNewFont(24)
     love.graphics.printf(self.text, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
 
     love.graphics.pop()
